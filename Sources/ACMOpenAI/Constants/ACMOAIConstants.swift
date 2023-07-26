014f477669d561c9a49f4a9f939c24e01014b43d
burak <burak.colak@appcent.mobi>
2023-07-26 13:45:36 +0300

    🎷 [BUGFIX] Content-type added.

--- Sources/ACMOpenAI/Constants/ACMOAIConstants.swift
+++ Sources/ACMOpenAI/Constants/ACMOAIConstants.swift
@@ -5,4 +5,6 @@
 /// Constants for holding open ai data
 struct ACMOAIConstants {
     static let organizationHeader = "OpenAI-Organization"
+    static let contentTypeKey = "Content-Type"
+    static let contentTypeValue = "application/json"
 }
