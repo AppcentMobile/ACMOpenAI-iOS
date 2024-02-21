2f9e7a603ecc5f1fd2a9419b5a136ab5c66c50c6
Burak Colak <burak.colak@appcent.mobi>
2024-02-21 16:15:42 +0300

    🎷 [UPDATE] Function calls support added.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/AppcentMobile/ACMNetworking.git", exact: "1.2.0"),
+        .package(url: "https://github.com/AppcentMobile/ACMNetworking.git", exact: "1.2.3"),
     ],
     targets: [
         .target(
